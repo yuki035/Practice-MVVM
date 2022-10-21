474b0c1a6d24b941cf389725f9d948dbd3e8c636
yuki035 <yukiyuki1918@icloud.com>
2022-10-21 22:02:38 +0900

    Initial Commit

---
+++ PracticeMVVM/PracticeMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeMVVMApp.swift
+//  PracticeMVVM
+//
+//  Created by 森祐樹 on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
