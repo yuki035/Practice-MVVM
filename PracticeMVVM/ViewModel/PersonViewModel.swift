d30e1382c8e72723278720e3c3f6bd1c1abd221f
yuki035 <yukiyuki1918@icloud.com>
2022-10-22 00:22:08 +0900

    add: ViewModelを作成

--- PracticeMVVM/ViewModel/PersonViewModel.swift
+++ PracticeMVVM/ViewModel/PersonViewModel.swift
@@ -5,4 +5,40 @@
 //  Created by 森祐樹 on 2022/10/21.
 //
 
-import Foundation
+import SwiftUI
+
+class PersonViewModel: ObservableObject {
+    @Published var people: [Person] = []
+    
+    init() {
+        addPeople()
+    }
+    
+    func addPeople() {
+        people = peopleData
+    }
+    
+    func shuffleOrder() {
+        people.shuffle()
+    }
+    
+    func reverseOrder() {
+        people.reverse()
+    }
+    
+    func removeLastPerson() {
+        people.removeLast()
+    }
+    
+    func removeFirstPerson() {
+        people.removeFirst()
+    }
+}
+
+let peopleData = [
+    Person(name: "田中", email: "Tanaka@email.com", phoneNumber: "555-5555"),
+    Person(name: "佐藤", email: "Sato@email.com", phoneNumber: "555-5555"),
+    Person(name: "斉藤", email: "Saito@email.com", phoneNumber: "555-5555"),
+    Person(name: "鈴木", email: "Suzuki@email.com", phoneNumber: "555-5555"),
+    Person(name: "松本", email: "Matsumoto@email.com", phoneNumber: "555-5555")
+]
