72c42981b3f8383401a28f0c602e039a02f4f091
yuki035 <yukiyuki1918@icloud.com>
2022-10-22 16:03:04 +0900

    add: スタメンと控え選手を分けて表示する機能を追加

--- PracticeMVVM/View/TeamView.swift
+++ PracticeMVVM/View/TeamView.swift
@@ -12,17 +12,9 @@ struct TeamView: View {
     var body: some View {
         ZStack(alignment: .bottom) {
             ScrollView() {
-                ForEach(Array(viewModel.team.enumerated()), id: \.element.id) { index, player in
-                    HStack {
-                        Text("\(index+1)")
-                        Text(player.name)
-                            .font(.title)
-                            .fontWeight(.bold)
-                        Text(player.position)
- 
-                    }
-                    .frame(height: 40)
-                    .padding()
+                HStack(alignment: .top, spacing: 80) {
+                    startingMemberView(array: viewModel.team)
+                    reserveMemberView(array: viewModel.team)
                 }
             }
             Menu("Menu".uppercased()) {
@@ -31,6 +23,43 @@ struct TeamView: View {
                 Button("Remove last", action: { viewModel.removeLastPerson()})
                 Button("Remove First", action: { viewModel.removeFirstPerson()})
                 Button("Reset", action: {viewModel.addteam()})
+            }
+        }
+    }
+}
+
+@ViewBuilder
+private func startingMemberView(array: [BaseballPlayer]) -> some View {
+    VStack {
+        Text("スタメン")
+        ForEach(Array(array.enumerated()), id: \.element.id) { index, player in
+            if (index < 9) {
+                HStack(spacing: 20) {
+                    Text("\(index+1)")
+                    Text(player.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text(player.position)
+                }
+                .frame(height: 40)
+                .padding()
+            }
+                
+        }
+    }
+}
+
+@ViewBuilder
+private func reserveMemberView(array: [BaseballPlayer]) -> some View {
+    VStack {
+        Text("控え選手")
+        ForEach(Array(array.enumerated()), id: \.element.id) { index, player in
+            if (index > 8) {
+                HStack {
+                    Text(player.name)
+                    Text(player.position)
+                }
+                .padding()
             }
         }
     }
