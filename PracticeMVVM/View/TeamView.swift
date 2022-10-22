03587c36dbf2ba0c1d9bc53c3329ebe24252a283
yuki035 <yukiyuki1918@icloud.com>
2022-10-22 11:00:12 +0900

    add: ファイルを追加

---
+++ PracticeMVVM/View/TeamView.swift
@@ -0,0 +1,43 @@
+//
+//  TeamView.swift
+//  PracticeMVVM
+//
+//  Created by 森祐樹 on 2022/10/22.
+//
+
+import SwiftUI
+
+struct TeamView: View {
+    @ObservedObject var viewModel = BaseballPlayerViewModel()
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            ScrollView() {
+                ForEach(viewModel.team) { player in
+                    HStack {
+                        Text("\(player.battingOrder ?? 0)")
+                        Text(player.name)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Text(player.position ?? "")
+ 
+                    }
+                    .frame(height: 40)
+                    .padding()
+                }
+            }
+            Menu("Menu".uppercased()) {
+                Button("Reverse", action: { viewModel.reverseOrder()})
+                Button("Shuffle", action: { viewModel.shuffleOrder()})
+                Button("Remove last", action: { viewModel.removeLastPerson()})
+                Button("Remove First", action: { viewModel.removeFirstPerson()})
+                Button("Reset", action: {viewModel.addteam()})
+            }
+        }
+    }
+}
+
+struct TeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamView()
+    }
+}
